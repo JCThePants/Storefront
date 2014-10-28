48283e509c16077799a931e5f87d832952dd87ec
jcThePants <jcthepants@gmail.com>
2014-10-27 17:53:50 -0700

    Implement localization and fix existing localization to match changes

--- src/com/jcwhatever/bukkit/storefront/Lang.java
+++ src/com/jcwhatever/bukkit/storefront/Lang.java
@@ -24,19 +24,15 @@
 
 package com.jcwhatever.bukkit.storefront;
 
-import com.jcwhatever.bukkit.generic.language.LanguageManager;
 import com.jcwhatever.bukkit.generic.language.Localized;
 
 public class Lang {
 
     private Lang() {}
 
-    private static LanguageManager _languageManager = new LanguageManager();
-
-
     @Localized
     public static String get(String text, Object... params) {
-        return _languageManager.get(Storefront.getInstance(), text, params);
+        return Storefront.getInstance().getLanguageManager().get(text, params);
     }
 
 }
